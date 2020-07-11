320bd66aaac838245dd87c83931776282e14461e
e <yalan.liu@macrowing.com>
2020-07-11 18:15:33 +0800

    添加ele

--- src/main.js
+++ src/main.js
@@ -3,6 +3,12 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import axios from 'axios'
+
+// 引入饿了么的组件
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+
+Vue.use(ElementUI);
 Vue.config.productionTip = false
     // Vue.prototype.$axios = axios;
 new Vue({
