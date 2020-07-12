e78975e698c72e26bfa011592ac4ac821b0597a1
e <yalan.liu@macrowing.com>
2020-07-13 07:38:41 +0800

    基础信息

---
+++ src/views/login/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="login">
+    <h1>登录</h1>
+  </div>
+</template>
+
+
+<script>
+// import { formatNum } from '../../toolkit'
+export default {
+  name: 'login',
+  methods: {}
+}
+</script>
