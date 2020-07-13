583fdfbe608b7aed6f21cd045e2c3a550d895f7c
zhangxu668 <1505046353@qq.com>
2020-07-13 09:02:36 +0800

    1

---
+++ src/components/commom/addcart.vue
@@ -0,0 +1,69 @@
+<template>
+    <div id="addcart" v-show="isshow">
+        <div class="addcart-body">
+            <div class="addcart-header">
+                <span>
+                    <button type="button" title="关闭" @click="handle">X</button>
+                </span>
+                 <h2>{{msg}}</h2>
+            </div>
+            <div class="addcart-content">
+                <div v-show="visible">loading...</div>
+                <div v-show="isshow">
+                    <p class="p10">目前选购商品共
+                        <span>1</span> 种
+                        <span>1</span> 件  
+                        合计：<span>¥100</span>
+                    </p>
+                    <div class="btnBar">
+                        <button class="cart_btn btn-default">继续购物</button>
+                        <button class="cart_btn btn-danger">去结算</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+   export default {
+        name: "addcart",
+        data(){
+            return {
+               msg:"成功加入购物车",
+               visible:false,
+               isshow:true
+            }
+        },
+        methods:{
+            handle(){
+                this.isshow=false;
+            }
+        },
+        created(){
+            this.$axios.interceptors.request.use( (config)=> {
+            // 在发送请求之前做些什么
+            this.visible=true;
+            this.isshow=false;
+            this.msg="正在加入购物车";
+            return config;
+            }, function (error) {
+            // 对请求错误做些什么
+            return Promise.reject(error);
+            });
+            this.$axios.interceptors.response.use( (response)=> {
+            // 对响应数据做点什么
+            console.log("响应拦截");
+            this.visible=true;
+            this.isshow=false;
+            this.msg="成功加入购物车";
+            return response;
+            }, function (error) {
+            // 对响应错误做点什么
+            return Promise.reject(error);
+            });
+        },
+   }
+</script>
+<style scoped>
+@import "~assets/css/addcart.css";
+</style>
---
+++ src/components/commom/addcollect.vue
@@ -0,0 +1,42 @@
+<template>
+    <div id="addcollect" v-show="isshow">
+        <div class="addcollect-body">
+            <button type="button" title="关闭" @click="handle">X</button>
+            <div class="addcollect-content">
+                <table>
+                    <tr>
+                        <td>
+                            <h3>收藏成功!</h3>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            您可以在 <a href="#" target="_blank">我的收藏</a> 中进行查看您还收藏了哪些商品
+                        </td>
+                    </tr>
+                </table>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "addcollect",
+        data(){
+            return {
+                 isshow:true
+            }
+        },
+        methods:{
+            handle(){
+                this.isshow=false;
+            }
+        },
+    }
+</script>
+<style scoped>
+@import "~assets/css/addcollect.css";
+    
+    
+</style>
