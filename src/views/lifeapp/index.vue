7088a7bb45c075d7775aaab7fb72367844ff6ff2
aprilchan7 <1chenyaozi>
2020-07-13 16:05:42 +0800

    修改

--- src/views/lifeapp/index.vue
+++ src/views/lifeapp/index.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="lifeapp">
-    <div class="name">
+    <!-- <div class="name">
       <span>
          <breadcrumbVue></breadcrumbVue>
       </span>
       <span style="margin-left:10px">商品筛选</span>
       <a href="">重置筛选条件</a>
-    </div>
+    </div> -->
     <div class="sort">
       <ul>
         <li>排序方式：</li>
