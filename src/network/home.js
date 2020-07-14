a802b7d6f8eb0f7719447c8661f946e63ed78e84
aprilchan7 <1chenyaozi>
2020-07-14 10:10:33 +0800

    mm

--- src/network/home.js
+++ src/network/home.js
@@ -24,7 +24,7 @@ export function getProductList() {
     return request({
         url: '/api/getProductList.do',
 
-        data: { PageCount: 100, CurrentPage: 1, key: '张三' },
+        //   data: { PageCount: 100, CurrentPage: 1, key: '张三' },
 
         data: { PageCount: 100, CurrentPage: 1, Key: '', P_Type_Menu_Id: 2 },
 
