1b0751ecadd6c512773ec69104a64fa90ea68dac
e <yalan.liu@macrowing.com>
2020-07-20 08:25:47 +0800

    1

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,7 @@
         <breadcrumbVue v-if="showbreadcrumb"></breadcrumbVue>
       </div>
     </div>
-    <router-view v-if="showRouter" />"
+    <router-view v-if="showRouter" />
     <sidebar />
     <div>
       <myfooter />
@@ -14,88 +14,88 @@
   </div>
 </template>
 <script>
-import shopMenu from '@/components/content/shopMenu'
-import breadcrumbVue from '@/components/content/breadcrumb'
-import { mapActions, mapGetters } from 'vuex'
-import productdeltails from '@/components/content/productdetails'
-import sidebar from '@/components/commom/sidebar'
-import person from '@/views/person/components/wode'
-import head from '@/views/person/components/head'
-import myfooter from '@/components/content/myfooter'
-import { isLogin } from './toolkit'
+import shopMenu from "@/components/content/shopMenu";
+import breadcrumbVue from "@/components/content/breadcrumb";
+import { mapActions, mapGetters } from "vuex";
+import productdeltails from "@/components/content/productdetails";
+import sidebar from "@/components/commom/sidebar";
+import person from "@/views/person/components/wode";
+import head from "@/views/person/components/head";
+import myfooter from "@/components/content/myfooter";
+import { isLogin } from "./toolkit";
 
 export default {
-  name: 'app',
+  name: "app",
   data() {
-    return {}
+    return {};
   },
   created() {},
   watch: {
     $route(to, from) {
       try {
         // console.log(to.path)
-        if (to.path.indexOf('person') > 0 || to.path.indexOf('order') > 0) {
+        if (to.path.indexOf("person") > 0 || to.path.indexOf("order") > 0) {
           if (!isLogin()) {
             this.$message({
-              message: '您还没有登录！请先登录',
-              type: 'warning'
-            })
+              message: "您还没有登录！请先登录",
+              type: "warning"
+            });
             setTimeout(() => {
               this.$router.push({
                 path: `/denglu`
-              })
-            }, 300)
+              });
+            }, 300);
           }
         }
 
-        let nav = this.$route.params.id
-        this.setNav(nav)
-        const header = document.querySelector('.header')
-        if (!header) return
-        document.querySelector('.header').className =
-          'header submenu-panel-hide'
+        let nav = this.$route.params.id;
+        this.setNav(nav);
+        const header = document.querySelector(".header");
+        if (!header) return;
+        document.querySelector(".header").className =
+          "header submenu-panel-hide";
         setTimeout(() => {
-          document.querySelector('.header').className = 'header'
-        }, 100)
+          document.querySelector(".header").className = "header";
+        }, 100);
       } catch (e) {
-        console.log(e)
+        console.log(e);
       }
     }
   },
   computed: {
     ishow: function() {
       if (
-        this.$route.path.indexOf('PerCenter') > 0 ||
-        this.$route.path.indexOf('denglu') > 0 ||
-        this.$route.path.indexOf('zhuce') > 0 ||
-        this.$route.path.indexOf('forgetPwd') > 0
+        this.$route.path.indexOf("PerCenter") > 0 ||
+        this.$route.path.indexOf("denglu") > 0 ||
+        this.$route.path.indexOf("zhuce") > 0 ||
+        this.$route.path.indexOf("forgetPwd") > 0
       ) {
-        return false
+        return false;
       } else {
-        return true
+        return true;
       }
     },
     showRouter: function() {
       return (
         // 等待菜单数据加载完成再显示 || 在订单页面显示
         this.$store.state.shopmenustore.menulist.length > 0 ||
-        this.$route.path.indexOf('PerCenter') > 0 ||
-        this.$route.path.indexOf('denglu') > 0 ||
-        this.$route.path.indexOf('zhuce') > 0 ||
-        this.$route.path.indexOf('forgetPwd') > 0
-      )
+        this.$route.path.indexOf("PerCenter") > 0 ||
+        this.$route.path.indexOf("denglu") > 0 ||
+        this.$route.path.indexOf("zhuce") > 0 ||
+        this.$route.path.indexOf("forgetPwd") > 0
+      );
     },
     showbreadcrumb() {
       return (
         this.$store.state.shopmenustore.menulist.length > 0 &&
         this.$store.state.shopmenustore.navbar &&
-        (this.$route.path.indexOf('home') == -1 ||
-          this.$route.path.indexOf('/') == 1)
-      )
+        (this.$route.path.indexOf("home") == -1 ||
+          this.$route.path.indexOf("/") == 1)
+      );
     }
   },
   methods: {
-    ...mapActions(['setNav'])
+    ...mapActions(["setNav"])
   },
   components: {
     shopMenu,
@@ -105,10 +105,10 @@ export default {
     person,
     myfooter
   }
-}
+};
 </script>
 <style lang="scss">
-@import '~assets/css/nomralize.min.css';
-@import '~assets/css/base.scss';
-@import '~assets/font/iconfont.css';
+@import "~assets/css/nomralize.min.css";
+@import "~assets/css/base.scss";
+@import "~assets/font/iconfont.css";
 </style>
