48dcaf1ab314da15ff5fb9e70c7ec1e7b6ce3cf2
aprilchan7 <1chenyaozi>
2020-07-21 11:06:53 +0800

    33

--- src/views/person/components/mysetion/pointsDetail.vue
+++ src/views/person/components/mysetion/pointsDetail.vue
@@ -1,10 +1,10 @@
 <template>
-  <div style="background-color:#EFEFEF;padding:30px;width:1000px">
-    <h4 style="color:#6B6B6B;text-align:left;margin-left:40px">积分变更记录</h4>
+  <div style="background-color:#EFEFEF;padding-top:30px;padding-left:50px;width:1000px">
+    <h4 style="color:#6B6B6B;text-align:left;margin-left:10px">积分变更记录</h4>
     <div >
     <el-table
-        :header-cell-style="{background:'#EFEFEF',fontSize:'14px',textAlign:'center'}"
-        :cell-style="{color:'#6B6B6B',background:'#EFEFEF',fontSize:'14px',textAlign:'center'}"
+        :header-cell-style="{background:'#EFEFEF',fontSize:'14px'}"
+        :cell-style="{color:'#6B6B6B',background:'#EFEFEF',fontSize:'14px'}"
         :data="myPoints"
         style="width: 100%;background-color:#EFEFEF;"
         >
@@ -14,7 +14,10 @@
         <el-table-column prop="name" label="类型" width="180">
         <template slot-scope="scope">{{scope.row.Message}}</template>
         </el-table-column>
-        <el-table-column prop="address" label="摘要" width="220">
+         <el-table-column prop="date" label="积分" width="150">
+        <template slot-scope="scope">{{ scope.row.Score}}</template>
+        </el-table-column>
+        <el-table-column prop="address" label="摘要" width="270">
         <template slot-scope="scope">{{scope.row.Content}}</template>
         </el-table-column>
         </el-table>
